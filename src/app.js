a6d5e9381256ed26a18bf3e54391ed2cbaee49f3
kame <holyturt@gmail.com>
2018-01-12 15:04:29 +0900

    Get firebase expenses on start

--- src/app.js
+++ src/app.js
@@ -3,9 +3,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
+import { addExpense, startSetExpenses } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
@@ -13,14 +12,14 @@ import './firebase/firebase';
 
 const store = configureStore();
 
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
   </Provider>
 );
 
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(() => {
+  ReactDOM.render(jsx, document.getElementById('app'));
+});
--- src/reducers/expenses.js
+++ src/reducers/expenses.js
@@ -20,6 +20,9 @@ export default (state = expensesReducerDefaultState, action) => {
             }
         });
 
+        case 'SET_EXPENSES':
+        return action.expenses;
+
         default: return state;
     }
 };
